be322bcc6b6661e46cf076631bccd8ba8113bf6f
Tsuyoshi Hirosawa <dmwtpdmwtp24@gmail.com>
2020-02-12 23:08:52 +0900

    chore: directory structure (#1)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,37 +1,12 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-sample-calendar
-      </h1>
-      <h2 class="subtitle">
-        My astonishing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    {{ 'hello' }}
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
 </script>
 
 <style>
