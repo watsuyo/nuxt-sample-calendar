66a0346f9fb3170c59ed5b16f43996c935551ce1
watsuyo <dmwtpdmwtp24@gmail.com>
2020-02-23 16:22:28 +0900

    filter

--- src/plugins/filter.ts
+++ src/plugins/filter.ts
@@ -1,35 +1,32 @@
 import Vue from 'vue'
-import * as dateformat from 'dateformat'
+import dateformat from 'dateformat'
 
-export function monthDate (
-  date: Date
-): string {
+export function date(date: Date): string {
+  const format = 'dd'
+  return dateformat(date, format)
+}
+
+export function monthDate(date: Date): string {
   const format = 'mm/dd'
   return dateformat(date, format)
 }
 
-export function yearMonth (
-  date: Date
-): string {
+export function yearMonth(date: Date): string {
   const format = 'yyyy/mm'
   return dateformat(date, format)
 }
 
-export function yearMonthDate (
-  date: Date
-): string {
+export function yearMonthDate(date: Date): string {
   const format = 'yyyy/mm/dd'
   return dateformat(date, format)
 }
 
-export function yearMonthDateTime (
-  date: Date
-): string {
+export function yearMonthDateTime(date: Date): string {
   const format = 'yyyy/mm/dd HH:MM'
   return dateformat(date, format)
 }
 
-const filters = { yearMonthDateTime, yearMonthDate, yearMonth, monthDate }
+const filters = [yearMonthDateTime, yearMonthDate, yearMonth, monthDate, date]
 
 Object.keys(filters).forEach((key) => {
   Vue.filter(key, filters[key])
