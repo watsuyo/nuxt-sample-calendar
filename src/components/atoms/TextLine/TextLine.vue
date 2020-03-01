24b75f08237cbe6134e8201aa739b4fefbf4a2e1
watsuyo <dmwtpdmwtp24@gmail.com>
2020-03-02 01:29:31 +0900

    fix: TextLine

--- src/components/atoms/TextLine/TextLine.vue
+++ src/components/atoms/TextLine/TextLine.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="default" :class="[fontColor, { bold: isBold }]">
+  <span class="default" :class="[fontColor, fontSize, { bold: isBold }]">
     <slot />
   </span>
 </template>
@@ -10,6 +10,12 @@ import Vue from 'vue'
 export default Vue.extend({
   props: {
     fontColor: {
+      type: String,
+      required: false,
+      default: ''
+    },
+
+    fontSize: {
       type: String,
       required: false,
       default: ''
@@ -27,6 +33,9 @@ export default Vue.extend({
 <style scoped>
 .default {
   font-size: 16px;
+}
+.fontL {
+  font-size: 24px;
 }
 .main {
   color: #21242b;
